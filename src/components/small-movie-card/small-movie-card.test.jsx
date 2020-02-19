43150aafefbe2db036f5d5737631bdce43105e87
ilataleksey <ilataleksey@gmail.com>
2020-02-19 23:25:01 +0300

    Создает тест и моки для теста для компонента SmallMovieCard

---
+++ src/components/small-movie-card/small-movie-card.test.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import SmallMovieCard from "./small-movie-card.jsx";
+import film from "./small-movie-card.test.mocks.js";
+
+it(`Render SmallMoviesCard`, () => {
+  const tree = renderer
+    .create(<SmallMovieCard
+      film={film}
+      onCardHover={() => {}}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
---
+++ src/components/small-movie-card/small-movie-card.test.mocks.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    name: `Наименование фильма`,
+    src: `img/film-poster.jpg`,
+  },
+  {
+    name: `Наименование фильма`,
+    src: `img/film-poster.jpg`,
+  },
+  {
+    name: `Наименование фильма`,
+    src: `img/film-poster.jpg`,
+  },
+  {
+    name: `Наименование фильма`,
+    src: `img/film-poster.jpg`,
+  },
+  {
+    name: `Наименование фильма`,
+    src: `img/film-poster.jpg`,
+  },
+  {
+    name: `Наименование фильма`,
+    src: `img/film-poster.jpg`,
+  },
+
+];
