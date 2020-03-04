e08d5dcade87ae6359bf333a0d7287c4c0f1aa4c
ilataleksey <ilataleksey@gmail.com>
2020-03-04 19:35:30 +0300

    Выполняет полный рендер DOM с помощью mount вместо shallow. Дополняет тест проверкой на пристановку видео

--- src/components/video-player/video-player.e2e.test.jsx
+++ src/components/video-player/video-player.e2e.test.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import VideoPlayer from "./video-player.jsx";
 import {film} from "../../mocks/mocks-test.js";
@@ -12,7 +12,7 @@ it(`Should video play`, () => {
   const isPlaying = true;
   const {poster, preview} = film;
 
-  const videoPlayer = shallow(
+  const videoPlayer = mount(
       <VideoPlayer
         isPlaying={isPlaying}
         poster={poster}
@@ -20,8 +20,20 @@ it(`Should video play`, () => {
       />
   );
 
-  videoPlayer.instance().setRef();
-  videoPlayer.instance().componentDidMount();
   expect(videoPlayer.state(`isPlaying`)).toBe(true);
+});
 
+it(`Should video pause`, () => {
+  const isPlaying = false;
+  const {poster, preview} = film;
+
+  const videoPlayer = mount(
+      <VideoPlayer
+        isPlaying={isPlaying}
+        poster={poster}
+        preview={preview}
+      />
+  );
+
+  expect(videoPlayer.state(`isPlaying`)).toBe(false);
 });
