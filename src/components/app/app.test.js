794411b3542370592fa14ff542b7f04b2025b58a
ilataleksey <ilataleksey@gmail.com>
2020-03-12 23:00:02 +0300

    Обновляет тест App

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,16 +1,32 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import App from "./app.jsx";
+import {App} from "./app.jsx";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
 import {films} from "../../mocks/mocks-test.js";
 
+const mockStore = configureStore([]);
+const ALL_GENRES = `All genres`;
+
 it(`Render App`, () => {
-  const tree = renderer.create(<App
-    films={films}
-  />, {
-    createNodeMock: () => {
-      return {};
-    }
-  }).toJSON();
+  const store = mockStore({
+    films,
+    filteredGenre: ALL_GENRES,
+  });
+
+  const tree = renderer.create(
+      <Provider store={store}>
+        <App
+          films={store.getState().films}
+          filteredGenre={store.getState().filteredGenre}
+          onGenreChange={() => {}}
+        />
+      </Provider>, {
+        createNodeMock: () => {
+          return {};
+        }
+      }
+  ).toJSON();
 
   expect(tree).toMatchSnapshot();
 });
--- src/mocks/mocks-test.js
+++ src/mocks/mocks-test.js
@@ -3,31 +3,37 @@ export const films = [
     name: `Наименование фильма-1`,
     poster: `img/film-poster.jpg`,
     preview: `video/film.mp4`,
+    genre: `genre-1`
   },
   {
     name: `Наименование фильма-2`,
     poster: `img/film-poster.jpg`,
     preview: `video/film.mp4`,
+    genre: `genre-1`
   },
   {
     name: `Наименование фильма-3`,
     poster: `img/film-poster.jpg`,
     preview: `video/film.mp4`,
+    genre: `genre-1`
   },
   {
     name: `Наименование фильма-4`,
     poster: `img/film-poster.jpg`,
     preview: `video/film.mp4`,
+    genre: `genre-2`
   },
   {
     name: `Наименование фильма-5`,
     poster: `img/film-poster.jpg`,
     preview: `video/film.mp4`,
+    genre: `genre-1`
   },
   {
     name: `Наименование фильма-6`,
     poster: `img/film-poster.jpg`,
     preview: `video/film.mp4`,
+    genre: `genre-2`
   },
 ];
 
@@ -35,5 +41,5 @@ export const film = {
   name: `Наименование фильма`,
   poster: `img/film-poster.jpg`,
   preview: `video/film.mp4`,
+  genre: `genre-1`
 };
-
