0e12194c3f8d443541b96f0bb97a1ecde2bfbe6a
ilataleksey <ilataleksey@gmail.com>
2020-03-12 22:18:14 +0300

    Создает файл reducer с описанием начального состояния, действий и функции reducer

---
+++ src/reducer.js
@@ -0,0 +1,53 @@
+import {extend} from "./utils.js";
+import films from "./mocks/films.js";
+
+const ALL_GENRES = `All genres`;
+
+const initialState = {
+  films,
+  filteredGenre: ALL_GENRES,
+};
+
+const ActionType = {
+  FILTER_UPDATE: `FILTER_UPDATE`,
+  GET_FILTERED_FILMS: `GET_FILTERED_FILMS`,
+};
+
+const ActionCreator = {
+  filterUpdate: (genre) => ({
+    type: ActionType.FILTER_UPDATE,
+    genre,
+  }),
+
+  getFilteredFilms: () => ({
+    type: ActionType.GET_FILTERED_FILMS,
+  }),
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.FILTER_UPDATE:
+      const newFilteredGenre = action.genre ? action.genre : state.filteredGenre;
+
+      return extend(state, {
+        filteredGenre: newFilteredGenre,
+      });
+
+    case ActionType.GET_FILTERED_FILMS:
+      let filteredFilms = films.filter((film) => {
+        return film.genre === state.filteredGenre;
+      });
+
+      if (state.filteredGenre === initialState.filteredGenre) {
+        return extend({}, initialState);
+      }
+
+      return extend(state, {
+        films: filteredFilms,
+      });
+  }
+
+  return state;
+};
+
+export {reducer, ActionCreator, ActionType, ALL_GENRES};
