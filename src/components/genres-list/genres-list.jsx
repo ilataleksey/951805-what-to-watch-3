3bb43b9b387cfc4b8334e7ea3d7d8f2c899a28c7
ilataleksey <ilataleksey@gmail.com>
2020-03-15 13:18:02 +0300

    Выносит колбек в отдельную функцию

--- src/components/genres-list/genres-list.jsx
+++ src/components/genres-list/genres-list.jsx
@@ -5,15 +5,17 @@ import {ALL_GENRES} from "../../const.js";
 const GenresList = (props) => {
   const {filteredGenre, genresList, onGenreChange} = props;
 
+  const onGenreClick = (evt) => {
+    evt.preventDefault();
+    onGenreChange(evt.target.textContent);
+  };
+
   return (
     <ul className="catalog__genres-list">
       <li className={`catalog__genres-item ${filteredGenre === ALL_GENRES ? `catalog__genres-item--active` : ``}`}>
         <a href="#"
           className="catalog__genres-link"
-          onClick={(evt) => {
-            evt.preventDefault();
-            onGenreChange(evt.target.textContent);
-          }}
+          onClick={onGenreClick}
         >{ALL_GENRES}</a>
       </li>
       {Array.from(genresList).map((genre, i) => {
